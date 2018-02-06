ab582f16358a600fc4ef7c18a19b3757177c9af0
pinksun77 <moonyellow@126.com>
2018-02-06 17:57:43 +0800

    start2

--- src/components/editableDiv.vue
+++ src/components/editableDiv.vue
@@ -3,28 +3,30 @@
     <div class="page-content container-fluid">
       <!--7777-->
       <div class="SB col-sm-12">
-        <div class="SBTitle col-sm-12">
-          <span class="col-sm-2">筛选条件</span>
-          <span class="col-sm-1 pull-right">
-            <button type="button" class="btn btn-default btn-sm">重置</button>
-          </span>
-          <span class="col-sm-1 pull-right">
-            <button type="button" class="btn btn-default btn-sm">查询</button>
-          </span>
-        </div>
-        <div class="col-sm-12">
-          <div class="col-sm-1 text-right">产品：</div>
-          <div class=" col-sm-4">
-            <select class="form-control" :disabled="produceChooseD">
-              <option value="1">行用卡偿贷1</option>
-              <option value="2">行用卡偿贷2</option>
-            </select>
+        <form class="form-horizontal" role="form">
+          <div class="SBTitle col-sm-12">
+            <span class="col-sm-2 control-label"><i class="ace-icon glyphicon glyphicon-search"></i>筛选条件</span>
+            <span class="col-sm-1 pull-right">
+              <button type="button" class="btn btn-default btn-sm">重置</button>
+            </span>
+            <span class="col-sm-1 pull-right">
+              <button type="button" class="btn btn-default btn-sm">查询</button>
+            </span>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-md-9" >.col-md-9 .col-md-push-3</div>
-          <div class="col-md-3" >.col-md-3 .col-md-pull-9</div>
-        </div>
+          <div class="col-sm-12 form-group">
+            <div class="col-sm-2 control-label">产品：</div>
+            <div class="col-sm-4">
+              <select class="form-control" :disabled="produceChooseD">
+                <option value="1">行用卡偿贷1</option>
+                <option value="2">行用卡偿贷2</option>
+              </select>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-9" >.col-md-9 .col-md-push-3</div>
+            <div class="col-md-3" >.col-md-3 .col-md-pull-9</div>
+          </div>
+        </form>
       </div>
 
     </div>
